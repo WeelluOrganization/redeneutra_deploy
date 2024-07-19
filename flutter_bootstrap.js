a9dc21c028b4027899d07569d91638d4f459a1c1
alerrandro borges <alerrandrokaton@gmail.com>
2024-07-19 11:27:08 -0300

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "371287503"
+    serviceWorkerVersion: "3716926846"
   }
 });
