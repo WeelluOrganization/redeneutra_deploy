d3971ed90cfeb24f5f5bcd98671b117db37fe8f0
alerrandro borges <alerrandrokaton@gmail.com>
2024-09-17 11:06:34 -0300

    build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4002621358"
+    serviceWorkerVersion: "2903967029"
   }
 });
