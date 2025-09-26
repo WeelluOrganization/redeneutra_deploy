dd55a1c33a6a94a06356d6e2a2c630cba3e919ea
Magno070 <magnomicrosoftj@gmail.com>
2025-09-26 15:35:30 -0300

    build web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3829689635"
+    serviceWorkerVersion: "1634157157"
   }
 });
