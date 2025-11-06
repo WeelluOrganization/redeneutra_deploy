0df7fc936765577dcbaf2d0b44f944d744223fb4
Magno070 <magnomicrosoftj@gmail.com>
2025-11-06 10:06:34 -0300

    build web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,14 +61,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "964ef1b3a56ab43f7d3eb4ac579e6bec
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.ico": "3a3959ac227b1a762cfb9dfad45701cc",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "b3b10c44e89bb7890a739ff32c1228e9",
+"flutter_bootstrap.js": "338dfdf0e503edbd4eb029fad5d6ad93",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "72a4f57fed690c7203477b8ec4f40390",
 "/": "72a4f57fed690c7203477b8ec4f40390",
-"main.dart.js": "50bd93af6caaa6999a333aa04b24a5af",
+"main.dart.js": "c1be6ca170f2727c7f4f95f1a9d02de5",
 "manifest.json": "5e6d5e4f1a5af15990ea36363b1c0c4f",
 "version.json": "a88af0618847413b0b66859eb1052ce2"};
 // The application shell files that are downloaded before a service worker can
